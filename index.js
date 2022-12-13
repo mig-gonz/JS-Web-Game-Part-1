a2af04df7d94254ed8b174e70254de37592f0cf7
Miguel <itsmig@proton.me>
2022-12-13 11:30:27 -0800

    refactored newItem() function code and added the sky and grass background to the document.

--- index.js
+++ index.js
@@ -1,51 +1,48 @@
+// background function
+function background(url, left, bottom, width, height) {
+  for (let i = 0; i < height; i++) {
+    for (let x = 0; x < width; x++) {
+      newImage(url, left + x * 100, bottom + i * 100);
+    }
+  }
+}
+
+// newImage function
 function newImage(path, left, bottom) {
   let img = document.createElement("img");
   img.src = path;
   img.style.position = "fixed";
-  img.style.left = left;
-  img.style.bottom = bottom;
+  img.style.left = left + "px";
+  img.style.bottom = bottom + "px";
   document.body.append(img);
+  return img;
 }
-function newItem(my, clown, shoes) {
-  let item = document.createElement("img");
-  item.src = my;
-  item.style.position = "fixed";
-  item.style.left = clown;
-  item.style.bottom = shoes;
-  document.body.append(item);
 
-  item.addEventListener("dblclick", function () {
+// newItem/eventListener function
+function newItem(path, left, bottom) {
+  let item = newImage(path, left, bottom);
+
+  item.addEventListener("dblclick", () => {
     item.remove();
   });
 }
 
-// new image function
+// invoking background function
+let height = window.innerHeight / 1.75;
+let Sky = window.innerHeight - height;
+let Grass = height;
+background("assets/sky.png", 0, height, window.innerWidth, Sky / 100);
+background("assets/grass.png", 0, 0, window.innerWidth, Grass / 100);
 
-// green character
-newImage("assets/green-character.gif", "100px", "100px");
+// invoking newImage function
+newImage("assets/green-character.gif", "100", "100");
+newImage("assets/pine-tree.png", "450", "200");
+newImage("assets/tree.png", "200", "300");
+newImage("assets/pillar.png", "350", "100");
+newImage("assets/crate.png", "150", "200");
+newImage("assets/well.png", "500", "425");
 
-// pine tree
-newImage("assets/pine-tree.png", "450px", "200px");
-
-// tree
-newImage("assets/tree.png", "200px", "300px");
-
-// pillar
-newImage("assets/pillar.png", "350px", "100px");
-
-// crate
-newImage("assets/crate.png", "150px", "200px");
-
-// well
-newImage("assets/well.png", "500px", "425px");
-
-//  invoking new item function
-
-//  sword
-newItem("assets/sword.png", "500px", "405px");
-
-//  shield
-newItem("assets/sheild.png", "165px", "185px");
-
-//  staff
-newItem("assets/staff.png", "600px", "100px");
+// invoking newItem funciton
+newItem("assets/sword.png", "500", "405");
+newItem("assets/sheild.png", "165", "185");
+newItem("assets/staff.png", "600", "100");
